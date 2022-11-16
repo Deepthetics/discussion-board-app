f726a6eeb688ba9ff37a62561bccc9d41d5c5b0f
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-11-16 21:33:12 +0200

    Add app.py, db.py, routes.py

---
+++ routes.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import render_template, request, redirect
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        pass
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        pass
+
+@app.route("/logout")
+def logout():
+    pass
