a559868872780905f60ea78f8acc0c3cb1338ab7
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-11-17 03:05:03 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,3 +4,22 @@ CREATE TABLE users (
     password TEXT, 
     role INTEGER
 );
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY, 
+    title TEXT
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY, 
+    title TEXT, 
+    created_at TIMESTAMP, 
+    user_id INTEGER REFERENCES
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY, 
+    title TEXT, 
+    created_at TIMESTAMP, 
+    user_id INTEGER REFERENCES
+);
